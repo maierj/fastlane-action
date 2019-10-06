3a96e571732792f08cc98bf80a9cabaae2cea830
Jonas Maier <maierjonas@live.de>
2019-10-06 16:22:39 +0200

    Refactor main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const core = require('@actions/core');
+const shell = require('shelljs');
 
 function run() {
     try {
         const lane = core.getInput('lane', { required: true });
 
-        console.log(`Executing lane ${lane}.`)
+        console.log(`Executing lane ${lane}.`);
+
+        shell.exec('echo $PWD');
+        shell.ls().forEach(function (file) {
+            console.log(file);
+        });
     } catch (error) {
         core.error(error);
         core.setFailed(error.message);
