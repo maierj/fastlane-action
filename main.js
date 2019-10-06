e02fc325c4ec803523929a64e6f5feea4593db7b
Jonas Maier <maierjonas@live.de>
2019-10-07 00:15:08 +0200

    Implemented option to specify relative path to subdirectory where fastlane folder is located.

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 const core = require('@actions/core');
 const shell = require('shelljs');
+const fs = require('fs');
 
 function run() {
     try {
         const lane = core.getInput('lane', { required: true });
+        const subdirectory = core.getInput('subdirectory', { required: false });
 
         console.log(`Executing lane ${lane}.`);
 
+        if (subdirectory) {
+            if (subdirectory.startsWith("/")) {
+                setFailed(new Error("Specified subdirectory path is not relative."));
+            }
+
+            if (fs.existsSync(`${process.cwd()}/${subdirectory}`)) {
+                shell.exec(`cd ${subdirectory}`);
+            } else {
+                setFailed(new Error("Specified subdirectory does not exist."));
+            }
+        }
+
         if (shell.exec(`fastlane ${lane}`).code !== 0) {
-            core.setFailed(`Executing lane ${lane} failed.`);
+            setFailed(new Error(`Executing lane ${lane} failed.`));
         }
     } catch (error) {
         core.error(error);
         core.setFailed(error.message);
     }
+}
+
+function setFailed(error) {
+    core.error(error);
+    core.setFailed(error.message);
 }
 
 run();
