4687eb676d1bab593141d023cbeee826381ddafc
Jonas Maier <maierjonas@live.de>
2019-10-06 15:27:22 +0200

    Setup basic project structure

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import * as core from "@actions/core";
+
+function run() {
+    try {
+        const lane = core.getInput('lane', { required: true });
+
+        console.log(`Executing lane ${lane}.`)
+    } catch (error) {
+        core.error(error);
+        core.setFailed(error.message);
+    }
+}
+
+run();
