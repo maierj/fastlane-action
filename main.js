c0d7507426e87f54741d945e50d5f251f4eb42b2
Jonas Maier <maierjonas@live.de>
2019-10-06 16:14:11 +0200

    Refactor main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function run() {
     try {
         const lane = core.getInput('lane', { required: true });
 
-        console.log("Executing lane " + lane + ".")
+        console.log(`Executing lane ${lane}.`)
     } catch (error) {
         core.error(error);
         core.setFailed(error.message);
