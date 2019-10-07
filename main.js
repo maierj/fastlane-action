f4a4de5dd39d99a455320f02b084e3b8df09ddf4
Jonas Maier <maierjonas@live.de>
2019-10-07 21:45:59 +0200

    Implemented github action input for lane options

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 function run() {
     try {
         const lane = core.getInput('lane', { required: true });
+        const optionsInput = core.getInput('options', { required: false });
         const subdirectory = core.getInput('subdirectory', { required: false });
 
         console.log(`Executing lane ${lane}.`);
@@ -12,6 +13,7 @@ function run() {
         if (subdirectory) {
             if (subdirectory.startsWith("/")) {
                 setFailed(new Error("Specified subdirectory path is not relative."));
+                return;
             }
 
             if (fs.existsSync(`${process.cwd()}/${subdirectory}`)) {
@@ -19,15 +21,41 @@ function run() {
                 shell.cd(subdirectory);
             } else {
                 setFailed(new Error(`Specified subdirectory ${subdirectory} does not exist.`));
+                return;
             }
         }
 
-        if (shell.exec(`fastlane ${lane}`).code !== 0) {
+        let deserializedOptions;
+        if (optionsInput) {
+            try {
+                deserializedOptions = JSON.parse(optionsInput);
+            } catch(e) {
+                setFailed(new Error(`Input value "options" cannot be parsed into a JSON object.`));
+                return;
+            }
+        } else {
+            deserializedOptions = {};
+        }
+
+        let fastlaneOptions = [];
+        for (let optionKey in deserializedOptions) {
+            if (Object.prototype.hasOwnProperty.call(deserializedOptions, optionKey)) {
+                fastlaneOptions.push(`${optionKey}:"${deserializedOptions[optionKey]}"`);
+            }
+        }
+
+        let fastlaneExecutionResult;
+        if (options.length === 0) {
+            fastlaneExecutionResult = shell.exec(`fastlane ${lane}`);
+        } else {
+            fastlaneExecutionResult = shell.exec(`fastlane ${lane} ${fastlaneOptions.join(" ")}`);
+        }
+
+        if (fastlaneExecutionResult.code !== 0) {
             setFailed(new Error(`Executing lane ${lane} failed.`));
         }
     } catch (error) {
-        core.error(error);
-        core.setFailed(error.message);
+        setFailed(error);
     }
 }
 
