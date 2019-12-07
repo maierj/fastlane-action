d04f086537c24d53e070bbb678051d7d83806c09
Jonas Maier <maierjonas@live.de>
2019-12-07 23:23:36 +0100

    Migrate tracking from firebase to cloud functions.

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -r node_modules
-rm package-lock.json
-
-npm install
-
-./node_modules/.bin/node-pre-gyp install --directory=./node_modules/grpc --target_platform=darwin --target_arch=x64 --target=12.12.0 --target_libc=unknown
-./node_modules/.bin/node-pre-gyp install --directory=./node_modules/grpc --target_platform=linux --target_arch=x64 --target=12.13.0 --target_libc=glibc
