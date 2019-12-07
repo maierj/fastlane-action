513e6a5a06f9ef3ef33b0c548f112d7bb1ace3a3
Jonas Maier <maierjonas@live.de>
2019-12-07 21:23:58 +0100

    fix node_modules

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,5 +5,5 @@ rm package-lock.json
 
 npm install
 
-./node_modules/.bin/node-pre-gyp install --directory=./node_modules/grpc --target_platform=darwin --target_arch=x64 --target=12.12.0 --target_libc=unknown
-./node_modules/.bin/node-pre-gyp install --directory=./node_modules/grpc --target_platform=linux --target_arch=x64 --target=12.13.0 --target_libc=glibc
+# ./node_modules/.bin/node-pre-gyp install --directory=./node_modules/grpc --target_platform=darwin --target_arch=x64 --target=12.12.0 --target_libc=unknown
+./node_modules/.bin/node-pre-gyp reinstall --directory=./node_modules/grpc --target_platform=linux --target_arch=x64 --target=12.13.0 --target_libc=glibc
