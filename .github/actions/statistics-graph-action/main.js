bc5ea952fdc0824adac37991563e049e2df088ab
Jonas Maier <maierjonas@live.de>
2019-12-26 21:56:17 +0100

    Change concept for gathering usage statistics

--- .github/actions/statistics-graph-action/main.js
+++ .github/actions/statistics-graph-action/main.js
@@ -1,30 +1,8 @@
 const core = require('@actions/core');
-const {google} = require('googleapis');
-
-const USER = "github.fastlane.action@gmail.com";
-
-function listMessages(apiKey) {
-    console.log("List messages");
-    const gmail = google.gmail({
-        version: "v1",
-        auth: apiKey,
-        labelIds: ["SENT"]
-    });
-
-    gmail.users.messages.list({
-        userId: USER
-    }).then(function(result) {
-        console.log(result);
-    }).catch(function(error) {
-        console.log("Failed to list messages: " + error);
-    });
-}
 
 function run() {
     try {
-        const gmailApiKey = core.getInput('gmail-api-key', { required: true });
-
-        listMessages(gmailApiKey);
+        console.log("test");
     } catch (error) {
         setFailed(error);
     }
