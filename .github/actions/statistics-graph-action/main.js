2bb62668b395a4143826abd0126d47967d23293a
Jonas Maier <maierjonas@live.de>
2019-12-26 13:21:11 +0100

    Fix listing api call

--- .github/actions/statistics-graph-action/main.js
+++ .github/actions/statistics-graph-action/main.js
@@ -26,8 +26,7 @@ function listMessages(client) {
     console.log("List messages");
     google.gmail("v1").users.messages.list({
         auth: client,
-        userId: "github.fastlane.action@gmail.com",
-        labelIds: ["SENT"]
+        userId: "github.fastlane.action@gmail.com"
     }).then(function(result) {
         console.log(result);
     }).catch(function(error) {
