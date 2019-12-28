64650966d0ce5d7f48c9960ea9c863c26ade8f8e
Jonas Maier <maierjonas@live.de>
2019-12-28 17:10:52 +0100

    Implement authentication using service account

---
+++ .github/actions/statistics-chart-action/main.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const {Firestore} = require('@google-cloud/firestore');
+
+function run() {
+    try {
+        const firebaseServiceAccountEmail = core.getInput("firebase-service-account-email", { required: true });
+        const firebaseServiceAccountPrivateKey = core.getInput("firebase-service-account-private-key", { required: true });
+
+        const firestore = new Firestore({
+            projectId: "github-fastlane-action",
+            credentials: {
+                client_email: firebaseServiceAccountEmail,
+                private_key: firebaseServiceAccountPrivateKey
+            }
+        });
+    } catch (error) {
+        setFailed(error);
+    }
+}
+
+function setFailed(error) {
+    core.error(error);
+    core.setFailed(error.message);
+}
+
+run();
